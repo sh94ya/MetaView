82ddd7f71aee0a146c0c3a98545fd0e3b0280e4a
sh94ya <yar5451@gmail.com>
2026-02-24 15:56:47 +0700

    DB connector add relative path to config

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     if not os.path.exists('logs'):
         os.mkdir('logs')
     if not os.path.exists('Logs.txt'):
-        open('logs\Logs.txt', 'a').close()
+        open(r'logs\Logs.txt', 'a').close()
     port = int(os.environ.get('PORT', 5000))
     app.run(host = '0.0.0.0', port = 5000, debug=False)
--- workspace/db_connect.py
+++ workspace/db_connect.py
@@ -3,10 +3,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
 from workspace.models.migration import create_tables
 import configparser
+import os
 
 #Read config file
+current_dir = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(current_dir, '..', 'config.ini')
+
 config = configparser.ConfigParser()
-config.read('.\\config.ini')
+config.read(config_path)
+
 db_string = "postgresql://{login}:{password}@{address}:{port}/{db}".format(login=config.get("DB", "login"),
                                                                            password=config.get("DB", "password"),
                                                                            address=config.get("DB", "address"),
