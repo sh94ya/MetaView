b72f39823958a67dcf6f71f0a6f01a98a3a4bcd1
sh94ya <yar5451@gmail.com>
2026-03-17 10:37:43 +0700

    Update Logger

--- main.py
+++ main.py
@@ -6,7 +6,5 @@ if __name__ == '__main__':
         os.mkdir('uploads')
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    if not os.path.exists('Logs.txt'):
-        open(r'logs\Logs.txt', 'a').close()
     port = int(os.environ.get('PORT', 5000))
     app.run(host = '0.0.0.0', port = 5000, debug=False)
--- workspace/logger.py
+++ workspace/logger.py
@@ -1,10 +1,18 @@
 import logging
+from datetime import datetime
+import os
+
+#Logs Path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+logs_dir = os.path.join(current_dir, '..\logs')
+log_file = os.path.join(logs_dir, f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log")
 
 logging.basicConfig(
     level=logging.DEBUG,
-    filename=".\\logs\Logs.txt",
+    filename=log_file,
     filemode='w',
     format="*** - %(levelname)s - %(asctime)s - %(module)s - %(funcName)s: %(lineno)d - %(message)s")
+
 
 def getLogger():
     logger = logging.getLogger('simple_example')
